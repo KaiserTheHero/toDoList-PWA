2c990027e9b5edcf422c6445c49290c0a0a1fb0f
Kaiser <vvaitkeviciuscardoso873@gmail.com>
2025-09-15 04:40:23 +0000

    Remoção do ícone dos códigos pois não entra no git

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = "pwa-tarefas";
-const FILES = ["/pwa-listaDeTarefas", "/pwa-listaDeTarefas/index.html", "/pwa-listaDeTarefas/toDoList.js", "/pwa-listaDeTarefas/manifest.json", "/pwa-listaDeTarefas/icons/icons512_maskable.png", "/pwa-listaDeTarefas/icons/icons512_rounded.png"];
+const FILES = ["/pwa-listaDeTarefas", "/pwa-listaDeTarefas/index.html", "/pwa-listaDeTarefas/toDoList.js", "/pwa-listaDeTarefas/manifest.json"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
 });
